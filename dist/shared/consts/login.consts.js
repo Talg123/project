919cd9fa2e3b7ff39e6314bdbc837f1ce43bedda
Tal <tal-vr@hotmail.com>
2019-06-21 16:52:52 +0300

    Removed dist

--- dist/shared/consts/login.consts.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginErrors = {
-    NO_SUCH_USER: "No such email",
-    WRONG_PASSWORD: "Wrong Password",
-    MISSING_PASSWORD_OR_EMAIL: "Email or Password is missing"
-};
-//# sourceMappingURL=login.consts.js.map
